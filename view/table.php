64d14cbcb664323f2108d029b47438ddd4047ff1
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-06-11 11:32:55 -0700

    revert changes.

--- view/table.php
+++ view/table.php
@@ -5,13 +5,13 @@ namespace Stanford\RepeatingReportRenderer;
 /** @var \Stanford\RepeatingReportRenderer\RepeatingReportRenderer $module */
 
 try {
-    //$module->processReport();
+    $module->processReport();
 //    $module->emLog($module->getHeaderColumns());
     echo json_encode(array(
         'status' => 'success',
-        'data' => $module->getReport(),
-        'columns' => array(),
-        'file' => ''
+        'data' => $module->getFinalData(),
+        'columns' => $module->getHeaderColumns(),
+        'file' => $module->getFileName()
     ));
 } catch (\LogicException $e) {
     echo json_encode(array('status' => 'error', 'message' => $e->getMessage()));
